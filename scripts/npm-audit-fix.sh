0b8492fca701a5aca035543f6e692a86ac0c7eff
Kate Sills <katelynsills@gmail.com>
2019-03-22 16:00:41 -0700

    add gitignore

--- scripts/npm-audit-fix.sh
+++ scripts/npm-audit-fix.sh
@@ -1,18 +1,14 @@
 git clone https://github.com/AgoricBot/test-repo.git
 cd test-repo
+git config user.email "kate+agoricbot@agoric.com"
+git config user.name "AgoricBot"
+git checkout -b npm-audit-fix
 
 if npm audit ; then
     echo "Nothing to fix"
 else
-    git config user.email "kate+agoricbot@agoric.com"
-    git config user.name "AgoricBot"
-    git checkout -b npm-audit-fix
-    npm audit fix
-    git add . 
-    git commit -m "results of running npm audit fix"
-    git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/test-repo.git
-    git push origin npm-audit-fix
-    hub pull-request --no-edit --base Agoric/test-repo:master
+  npm audit fix
+  files_changed=true
 fi
 
 # Do the same thing with the package.json in the integration-test folder
@@ -20,16 +16,14 @@ cd integration-test
 if npm audit ; then
     echo "Nothing to fix"
 else
-    git config user.email "kate+agoricbot@agoric.com"
-    git config user.name "AgoricBot"
-    git checkout -b npm-audit-fix
-    sudo npm cache clean -f
-    npm install
-    npm ci
-    npm audit fix
-    git add . 
-    git commit -m "results of running npm audit fix"
-    git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/test-repo.git
-    git push origin npm-audit-fix
-    hub pull-request --no-edit --base Agoric/test-repo:master
+  npm audit fix
+  files_changed=true
+fi
+
+if [ "$files_changed" = true ] ; then
+  git add . 
+  git commit -m "results of running npm audit fix"
+  git remote set-url origin https://AgoricBot:$GITHUB_TOKEN@github.com/AgoricBot/test-repo.git
+  git push origin npm-audit-fix
+  hub pull-request --no-edit --base Agoric/test-repo:master
 fi
